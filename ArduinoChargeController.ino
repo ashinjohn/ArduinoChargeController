843086f1dca3c12d67609ae464edd8f578580225
ashinjohn <ashinbasiljohn@gmail.com>
2020-06-03 17:06:23 +0530

    Arduino File Initialisation

---
+++ ArduinoChargeController.ino
@@ -0,0 +1,19 @@
+  
+/*
+   Arduino Charge Controller 
+   Description: Arduino based charge controller for Solar Systems that Regulates the Output Voltage of XL4016 Buck Regulator.
+   Firmware fine tuned for 100W Solar Panel and 12V Lead Acid Battery
+   IDE -> Arduino
+   MCU - Arduino Uno R3 with Custom Shield for measuring voltage and adjusting Battery Charging Voltage
+   Revision- 1.0
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
