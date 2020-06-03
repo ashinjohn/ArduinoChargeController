dc3e4877bbf71627fdcb4dec6da898fa1de22446
ashinjohn <ashinbasiljohn@gmail.com>
2020-06-03 20:10:25 +0530

    Added LED and OUTPUTS Library with Initialisation

--- ArduinoChargeController.ino
+++ ArduinoChargeController.ino
@@ -1,19 +1,26 @@
-  
+
 /*
-   Arduino Charge Controller 
+   Arduino Charge Controller
    Description: Arduino based charge controller for Solar Systems that Regulates the Output Voltage of XL4016 Buck Regulator.
    Firmware fine tuned for 100W Solar Panel and 12V Lead Acid Battery
    IDE -> Arduino
    MCU - Arduino Uno R3 with Custom Shield for measuring voltage and adjusting Battery Charging Voltage
-   Revision- 1.0
+
+   Created by abjohn.  June 3, 2020.
 */
 
-void setup() {
-  // put your setup code here, to run once:
+#include "src/LED.h"
+#include "src/OUTPUTS.h"
 
+/* PIN INITIALISATION */
+LED Red(13);
+LED Blue(12);
+OUTPUTS BUZZER(9);
+OUTPUTS FAN(10);
+
+void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
